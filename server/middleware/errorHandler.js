b1f9cdbfb39a785f11ce3971ffca3131857d5bc4
Leonel Curti <leonelcurti@gmail.com>
2021-04-07 20:36:47 -0300

    Gracefully shuts down the server functionality and minor fixes in express errorhandler

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -2,14 +2,18 @@ const errorHandler = (err, req, res, next) => {
   // Operational, trusted error: send message to client
   if (err.isOperational) {
     res.status(err.statusCode || 500).json({
-      success: false,  
+      success: false,
       message: err.message || "Something went wrong!",
     });
   } else {
-    //log to console for dev  
+    // Programming or other unknown error
     console.log("----ERROR--------------".red);
-    console.log(err.stack);
+
+    console.log(`Error name: ${err.name}\n`);
+    console.log(`Error message: ${err.message}\n`);
+
     console.log("------------------------".red);
+
     res.status(500).json({
       success: false,
       message: "Something went wrong!",
